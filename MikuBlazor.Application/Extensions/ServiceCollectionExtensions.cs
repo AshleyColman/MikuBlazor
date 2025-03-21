33ad90bf3d286d8bf460364d3b22feb600f25f3b
Ashley <AshleyPC@DESKTOP-6VKB937>
2025-03-21 21:47:28 +0000

    wip: add validation behavior

--- MikuBlazor.Application/Extensions/ServiceCollectionExtensions.cs
+++ MikuBlazor.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using MikuBlazor.Application.AutoMapper;
+using MikuBlazor.Web.Middleware;
 
 namespace MikuBlazor.Application.Extensions;
 
@@ -9,12 +11,17 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(
         this IServiceCollection services)
     {
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        
         services.AddMediatR(options =>
         {
-            options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            options.RegisterServicesFromAssembly(currentAssembly);
+            options.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
         });
 
         services.AddAutoMapper(options => options.AddProfile<AppProfile>());
+
+        services.AddValidatorsFromAssembly(currentAssembly);
         
         return services;
     }
